7cb3d89041574f5d494eb148c4f09a0dce1318db
Buckley Robinson <brobinson@verticalresponse.com>
2013-05-07 09:37:59 -0700

    skeleton jasmine tests

--- test/jasmine-spec.js
+++ test/jasmine-spec.js
@@ -6,5 +6,28 @@ describe("Person", function () {
 	afterEach(function () {
 
 	});
-	
+	 
+  describe("object factory", function(){
+    it('creates an object', function(){
+    });
+  });
+
+
+  describe("event emmitter", function(){
+    beforeEach(function(){
+
+    });
+
+    it("subscribes to events", function(){
+      fail();
+    });
+
+    it("emits events", function(){
+      fail();
+    });
+
+    it("unsubscribes", function(){
+      fail();
+    });
+  });
 });
